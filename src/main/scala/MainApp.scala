f31fe57aa03599f056de210cee0e8f1c12186c60
changzhiwin <changzhiwin@gmail.com>
2022-06-25 12:03:14 +0800

    Clean Main

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -15,20 +15,4 @@ object MainApp {
 
     SimpleSelect.run(whichCase)
   }
-
-  def notPractice(df: DataFrame): Unit = {
-    val updatedDF = df.withColumn("OtherInfo", 
-      struct(
-        col("id").as("identifier"),
-        col("gender").as("gender"),
-        col("salary").as("salary"),
-        when( col("salary").cast(IntegerType) < 2000, "Low").
-        when( col("salary").cast(IntegerType) < 4000, "Medium").
-        otherwise("High").alias("Salary_Grade")
-      )
-    ).drop("id","gender","salary")
-
-    updatedDF.printSchema()
-    updatedDF.show(false)
-  }
 }
